e0339020c4e1d5db8b390753ce58dc7f825ac0e2
Ivan Humeniuk <rt072@tutanota.com>
2019-03-24 07:13:45 +0000

    Change type of file from symlink to file

--- autostart.sh
+++ autostart.sh
@@ -1,1 +1,42 @@
-/home/rt072/.dwm/autostart.sh
+#!/bin/bash 
+compton --config ~/.config/compton/compton.conf &
+#nitrogen --restore &
+pasystray &
+#urxvtd -q -o -f &
+/home/rt072/.scripts/chwall &
+
+dte(){
+  dte="$(date +"%A, %d %B |  %l:%M%p")"
+  echo -e " $dte"
+}
+
+#upd(){
+#  upd=`checkupdates | wc -l`
+#  echo -e "⟳  $upd updates"
+#}
+
+mem(){
+  mem=`free | awk '/Mem/ {printf "%d MiB\n", $3 / 1024.0}'`
+  echo -e " $mem"
+}
+
+cpu(){
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 1
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e " $cpu% cpu"
+}
+
+bat(){
+  bat=$(acpi -b | cut -d"," -f2 | cut -d" " -f2)
+  echo -e " $bat"
+}
+
+
+while true; do
+    xsetroot -name "$(cpu) | $(bat) | $(mem) | $(dte)"
+     sleep 5s    # Update time every ten seconds
+done &
