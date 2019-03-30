8e6c07c231e14a8bffc06460ed11def8de5e8b0d
Ivan Humeniuk <rt072@tutanota.com>
2019-03-30 19:06:35 +0000

    doanload and apply successful cyclelayout patch

--- config.h
+++ config.h
@@ -65,6 +65,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
     { "HHH",      grid },
+    { NULL,       NULL },
 };
 
 /* key definitions */
@@ -103,6 +104,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,       setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,       setlayout,      {.v = &layouts[2]} },
     { MODKEY,                       XK_g,       setlayout,      {.v = &layouts[3]} },
+    { MODKEY|ControlMask,           XK_comma,   cyclelayout,    {.i = -1 } },
+    { MODKEY|ControlMask,           XK_period,  cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,   setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,   togglefloating, {0} },
 	{ MODKEY,                       XK_0,       view,           {.ui = ~0 } },
