7fa5666d1295ed087167fab86448ed752dd3888c
Ivan Humeniuk <rt072@tutanota.com>
2019-03-31 07:33:44 +0000

    Apply succesfull rotatestack patch

--- config.h
+++ config.h
@@ -92,6 +92,8 @@ static Key keys[] = {
 	{ Mod1Mask,             	    XK_F2,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,		                XK_Return,  spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,       togglebar,      {0} },
+    { MODKEY|ShiftMask,             XK_j,       rotatestack,    {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_k,       rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_j,       focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,       focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,       incnmaster,     {.i = +1 } },
