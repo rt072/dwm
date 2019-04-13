4b55cddc69da56898801c6fcad3b316777661b91
Ivan Humeniuk <rt072@tutanota.com>
2019-04-13 21:36:52 +0000

    comment 'zoom' function in hotkeys

--- config.h
+++ config.h
@@ -100,7 +100,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,       incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,       setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,       setmfact,       {.f = +0.05} },
-	{ Mod1Mask,                     XK_Return,  zoom,           {0} },
+	// { Mod1Mask,                     XK_Return,  zoom,           {0} },
 	{ MODKEY,                       XK_Tab,     view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,       killclient,     {0} },
 	{ MODKEY,                       XK_t,       setlayout,      {.v = &layouts[0]} },
